d59fb0a2d6ffec5ea78a0862428c14491b1fb976
Anish-Thakur291 <anishthakur404@gmail.com>
2025-02-04 20:55:51 +0530

    Time: 4 ms (53.39%), Space: 9.8 MB (67.03%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,37 +2,27 @@ class Solution {
 public:
     string countAndSay(int n) {
         // Base case: if n is 1, return the initial sequence "1"
-        if (n == 1) return "1";
-        
-        // Start with the first sequence
-        string s = "1";
+    if(n==1) return "1";
+    if(n==2) return "11";
+    
+    string ans="11";
+    for(int i=3;i<=n;i++){
+        string temp="";
+        ans+="&";
+        int count=1;
+        for(int j=1;j<ans.size();j++){
+            if(ans[j-1]!=ans[j]){
+                temp+=to_string(count);
+                temp+=ans[j-1];
+                count=1;
 
-        // Generate the sequence iteratively up to the nth sequence
-        for (int i = 2; i <= n; i++) {
-            const int len = s.size(); // Length of the current sequence
-            string t; 
-            t.reserve(len << 1); // Reserve space to minimize reallocations
-
-            // Process the current sequence to build the next one
-            for (int j = 0; j < len;) {
-                char c = s[j]; // Current character to count
-                int count = 1; // Start counting occurrences
-
-                // Count consecutive characters
-                while (j + count < len && s[j + count] == c) count++;
-
-                // Append the count and the character to the new sequence
-                t.push_back('0' + count); // Convert count to character
-                t.push_back(c);
-
-                // Move to the next character group
-                j += count;
             }
-            // Update the current sequence to the newly generated one
-            s = move(t);
+            else{
+                count++;
+            }
         }
-
-        // Return the nth sequence
-        return s;
+        ans=temp;
+    }
+    return ans;
     }
 };
