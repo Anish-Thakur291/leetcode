d4aeda4a5729abd8f0c0d0b22f020d0762ecc92f
Anish-Thakur291 <anishthakur404@gmail.com>
2025-02-22 15:56:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,62 +1,73 @@
 class MyCircularQueue {
-private:
-    int* arr;
+    int *arr;
     int front;
     int rear;
     int size;
-
 public:
     MyCircularQueue(int k) {
-        size = k;
-        arr = new int[size];
-        front = -1;
-        rear = -1;
+        size=k;
+        arr=new int[size];
+        front=-1;
+        rear=-1;
     }
-
+    
     bool enQueue(int value) {
-        if (isFull()) {
+        if((front==0 && rear==size-1) || (rear==(front-1)%(size-1))){
+           
             return false;
-        } else if (isEmpty()) {
-            front = rear = 0;
-        } else {
-            rear = (rear + 1) % size;
         }
-        arr[rear] = value;
+        else if(front==-1){
+            front=rear=0;
+           
+        }
+        else if(rear==size-1 && front!=0){
+            rear=0;
+            
+        }
+        else{
+            rear++;
+            
+        }
+        arr[rear]=value;
         return true;
     }
-
+    
     bool deQueue() {
-        if (isEmpty()) {
+        if(isEmpty()){
             return false;
         }
-        if (front == rear) {
-            front = rear = -1;
-        } else {
-            front = (front + 1) % size;
-        }
+        if(front==rear){
+            front=rear=-1;
+        }  
+        else {
+            front=(front+1)%size;
+            
+        } 
         return true;
     }
-
+    
     int Front() {
-        if (isEmpty()) {
-            return -1;
+        if(isEmpty()){
+            return false;
         }
-        return arr[front];
+       return arr[front];
     }
-
+    
     int Rear() {
-        if (isEmpty()) {
-            return -1;
+         if(isEmpty()){
+            return false;
         }
-        return arr[rear];
+       return arr[rear];
     }
-
+    
     bool isEmpty() {
-        return (front == -1);
+       return (front==-1);
+        
     }
-
+    
     bool isFull() {
-        return ((rear + 1) % size == front);
+      return ((rear + 1) % size == front);
+        
     }
 };
 
