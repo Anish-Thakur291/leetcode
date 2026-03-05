04005e74e9f2bbbb464a47764c2eebb5f5c111c9
Anish Thakur <anishthakur404@gmail.com>
2026-03-05 14:39:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,9 +4,10 @@ class Solution:
         r=len(height)-1
         max_stored_water=0
         while l<r:
-            bar= min(height[l],height[r])
-            water=r-l
-            max_stored_water=max(max_stored_water,bar*water)
+            # bar= min(height[l],height[r])
+            # water=r-l
+            # max_stored_water=max(max_stored_water,bar*water)
+            max_stored_water=max(max_stored_water,min(height[l],height[r])*r-l)
             if height[l]<=height[r]:
                 l+=1
             else:
